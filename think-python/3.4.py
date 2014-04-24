226104ab8299230574b2b37670b4e754df030df5
Jason Stephens <jason@catalogspree.com>
2014-04-23 18:00:10 -0700

    Function Objects exercise 3.4

---
+++ think-python/3.4.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+'''
+Function Objects
+can be assigned to variables
+or passed as an argument.
+1. write and test the example script
+2. modify do_twice so that it:
+	- takes two arguments, a function object and a value
+	- passing the value as an argument.
+'''
+
+'''
+#1
+def do_twice(f):
+	f()
+	f()
+
+def print_spam():
+	print 'spam'
+
+do_twice(print_spam)
+'''
+
+#2
+def do_twice(f, strArg, valArgument):
+	f(strArg, valArgument)
+
+def print_spam(strArg, valArgument):
+	print (strArg)*valArgument
+
+do_twice(print_spam, 'What is this ' ,10)
+
+
+
+
+
