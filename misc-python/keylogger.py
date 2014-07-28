3413ea4c1713fff789e3a9b6be9883d3e6f3d0f8
Jason Stephens <jason@catalogspree.com>
2014-07-27 21:06:17 -0700

    completed keylogger mimicker

--- misc-python/keylogger.py
+++ misc-python/keylogger.py
@@ -1,36 +1,15 @@
 #!/usr/bin/env python -tt
 
 """jstephens - pyfu - 2014 july
-Process the output from my keylogger:
-- /Volumes/SubThree/Dropbox/documents/logKext_keyLogger/out_logFile.txt
-1. convert to single string
-2. remove unnecessary chars
-
-3. Mimic? using mimic module
-4. ??
-- remove extra chars using the wourdcount2 module's remove_char function
-- can mimic the keylogger using the mimic_dict function from mim2 module
-
-usage:
-
+Either counts the words or mimics the writting style from the keylogger text
 """
 
 import sys
-
-def convert_file_to_file_object(filename):
-	"""Takes a text file and returns a single string
-	"""
-
-	# to track the commmand line instructions
-	f = open(filename, 'r')
-	text = f.read()        # create a SINGLE string
-
-
-	return text
+import random
 
 
 def convert_keylog_text(filename):
-	"""Manage word count process.
+	"""Manage word count process.  From file, print word count to console.
 	- Outsource file conversion
 	- Outsource char-removal and word-count to other functions
 	- Outsource text splitting 
@@ -58,69 +37,18 @@ def convert_keylog_text(filename):
 			print k, d[k]
 		#print "%s  =>  %s" % (pair)
 
-
-
-	#print splitTextListed
-	#for section in splitText:
-	#	print section
-
-	#split cleanedText by new line.  creates a list where eacg eleemtbn is line.
-
-
-
 	sys.exit(0)
 
-	# count
-
-	# count top
-
-	# mimic using random sample from a word's NEXT words
-
-
-def count_words(splitTextListed):
-	"""Count number of words in a list of strings, each string a group of words
-	"""
-	wordList = []
-
-	d = {}
-
-	# 1. currently words are grouped by newline and listed in splitTextListed
-	# 2. for each ground of words, split into a list of words.
-	# 3. then iterate through list of words from group and append every item to master list
-	for groupedString in splitTextListed:
-		# take each group of words and split according to whitespace
-		splitGroup = groupedString.split()
-		# iterate through each word in the group and append that word to a master list
-		for word in splitGroup:
-			if word != None:
-				wordList.append(word)
-
-
-	#print wordList
-
-	for word in wordList:
-		# first standardize cases
-		word = word.lower()
-		# look for the word in the dicts keys.  if not there, add it.  if it is, add 1 to value
-		if word in d:
-			d[word] += 1
-		else:
-			d[word] = 1
-
-	return d
-
-
-
-
-def split_text(cleanedText):
-	"""Split a string by newline.  Return a list containing sections by newline.
+def convert_file_to_file_object(filename):
+	"""Takes a text file and returns a single string
 	"""
 
+	# to track the commmand line instructions
+	f = open(filename, 'r')
+	text = f.read()        # create a SINGLE string
 
 
-	splitText = cleanedText.split('\n')
-	return splitText
-
+	return text
 
 def remove_char(stringedText):
 	""" Scrub string of unnecessary chars.  Return a new clean string
@@ -138,7 +66,12 @@ def remove_char(stringedText):
 	# 2. Iterate through the list when analyzing each char
 	# 3. recurs as before
 
-	listOfSpecialChars = ['<del>', '<esc>', '<cntrl>', '<tab>', '<cmd>', '<right>', '<left>', '<down>', '<up>', '<shift>']
+	# if it's Alice.txt, then use the full list.  otherwise use shorter list
+	if sys.argv[2] == '-2':
+		listOfSpecialChars = [',', '.', '?', '!', '`', '<del>', '<esc>', '<cntrl>', '<tab>', '<cmd>', '<right>', '<left>', '<down>', '<up>', '<shift>']
+	else:
+		listOfSpecialChars = ['<del>', '<esc>', '<cntrl>', '<tab>', '<cmd>', '<right>', '<left>', '<down>', '<up>', '<shift>']
+
 
 	for item in listOfSpecialChars:
 		if item in text:
@@ -146,86 +79,160 @@ def remove_char(stringedText):
 			remove_char(text)     # recursively call until item no longer in text, then fail condition
 	return text
 
+def split_text(cleanedText):
+	"""Split a string by newline.  Return a list of words.
 	"""
-	if '<del>' in text:
-		text = text.replace('<del>', '')
-		remove_char(text)
-	if '<esc>' in text:
-		text = text.replace('<esc>', '')
-		remove_char(text)
-	if '<cntrl>' in text:
-		text = text.replace('<cntrl>', '')
-		remove_char(text)
-	if '<tab>' in text:
-		text = text.replace('<tab>', '')
-		remove_char(text)
-	if '<cmd>' in text:
-		text = text.replace('<cmd>', '')
-		remove_char(text)
-	if '<right>' in text:
-		text = text.replace('<right>', '')
-		remove_char(text)
-	if '<left>' in text:
-		text = text.replace('<left>', '')
-		remove_char(text)
-	if '<up>' in text:
-		text = text.replace('<up>', '')
-		remove_char(text)
-	if '<down>' in text:
-		text = text.replace('<down>', '')
-		remove_char(text)
-	if '<shift>' in text:
-		text = text.replace('<shift>', '')
-		remove_char(text)
-	#if '(' in text:
-	#	text = text.replace('(', '')
-	#	remove_char(text)
-	#if ')' in text:
-	#	text = text.replace(')', '')
-	#	remove_char(text)
-	#if '[ ]' in text:
-	#	text = text.replace('[ ]', '[]')
-	#	remove_char(text)
-	return text
+
+	wordList = []
+
+	splitTextListed = cleanedText.split('\n')
+	
+
+	for groupedString in splitTextListed:
+		# take each group of words and split according to whitespace
+		splitGroup = groupedString.split()
+		# iterate through each word in the group and append that word to a master list
+		for word in splitGroup:
+			if word != None:
+				wordList.append(word)
+
+	return wordList
+
+def count_words(wordList):
+	"""Count number of words in a list of strings.  Return dict with word = count
 	"""
+
+	d = {}
+
+	for word in wordList:
+		# first standardize cases
+		word = word.lower()
+		# look for the word in the dicts keys.  if not there, add it.  if it is, add 1 to value
+		if word in d:
+			d[word] += 1
+		else:
+			d[word] = 1
+
+	return d
+
+def create_mimic_dict(wordList):
+	"""Create and return a dict listing all words that follow each key word from a string
+	"""
+	d = {}
+	prev = ''
+	
+	for word in wordList:
+		if not prev in d:
+			d[prev] = [word]
+		else:
+			d[prev].append(word) 
+		prev = word
+
+	return d
+
+def print_mimic_wordlists(mimic_dict):
+	"""Print popular words followed by a list of all the words which follow that popular word.
+	Define popular as any word used more than 10 times.
+	"""
+
+	for key in mimic_dict.keys():
+		# If the word is used more than 10x, then it should have more than 10 words in it's mimic list.
+		if len(mimic_dict[key]) > 10:
+
+			print "===== '%s' ====> followed by" % key
+			for item in mimic_dict[key]:
+				print item, "",
+			print "\n"
+
+def create_mimic_text(mimic_dict):
+	"""Create a new text that mimics the style of the original text.
+	"""
+	#f = open('mimic_text.txt', 'r+')
+	f = open("/Volumes/SubThree/Dropbox/documents/logKext_keyLogger/out_logFile_mimicked.txt", "r+")
+
+	s = ''
+	
+	for keyword in mimic_dict.keys():
+		randNext = random.choice(mimic_dict[keyword])
+		print keyword, randNext,
+		s = " " + keyword + " " + randNext
+		#f.writelines(keyword)
+		#qf.writelines(randNext)
+		f.writelines(s)
+		#s = str(s + keyword + randNext),
+	f.seek(0)
+
+	f.close()
 
 def mimic_style(filename):
 	"""Mimic writing style of a text file.
 	Take a text file and return a text of same length written with similar style
 	Select randomly one word from the list of words that follows every word.
 	"""
-	print 'made it to mimic_style function'
+	#print 'made it to mimic_style function'
 
+	# Convert file to single string
+	singleStringOfText = convert_file_to_file_object(filename)
+
+	# Clean the text
+	cleanedString = remove_char(singleStringOfText)
+
+	wordList = split_text(cleanedString)
+
+	#print wordList
+	mimic_dict = create_mimic_dict(wordList)
+
+	# ask for user input.  If print dict or if mimic text?
+	ans = raw_input('print mimicked words? y/n: ')
+	if ans == 'y':
+		# print the mimmicked words:
+		print_mimic_wordlists(mimic_dict)
+
+	ans = raw_input('create mimicked text? y/n: ')
+	if ans == 'y':
+		create_mimic_text(mimic_dict)
+	else:
+		sys.exit(0)
+	
+
+	sys.exit(0)
+	#cleanedText = remove_char()
 
 def main():
 	"""Take command line args.  If filename is not specified,
 	then resort to default keylog text file.
 	"""
 
-	assignDefault = False
-	filename = ''
-
-
 	# IF no command line args, assing defulat
 	# if there are not 2 args from command line, or if 2 arg isn't valid then defualt to keylog text
+	filename = ''
+	usage = "Usage:  ./keylogger.py {--count | --mimic } { -1 = keylog text  | -2 = alice in wonderland }"
 	if len(sys.argv) != 3:
-		print "Usage:  ./keylogger.py {--count | --mimic } filename"
-		print " ========> Defaults to a count of words in keylogger text"
-		assignDefault = True
+		print usage
+		sys.exit(1)
 
-	if assignDefault == True:
+	option = sys.argv[1]
+	fileOption = sys.argv[2]
+
+	if fileOption == '-1':
 		filename = "/Volumes/SubThree/Dropbox/documents/logKext_keyLogger/out_logFile.txt"
-	# if a filename is added in command line, then allow it to overwrite the  default
+	elif fileOption == '-2':
+		filename = "/jayjaycody/mypy/google-py/basic/alice.txt"	
 	else:
-		filename = sys.argv[2]
+		print "unknown fileOption" + fileOption
+		print usage
+		sys.exit(1)
 
-	if sys.argv > 2:
-		if sys.argv[1] == '--count':
-			convert_keylog_text(filename)
-		elif sys.argv[1] == '--mimic':
-			mimic_style(filename)
-	else:
+
+	if option == "--count":
+		print "Counting words from file: \n  " + filename
 		convert_keylog_text(filename)
+	elif option == "--mimic":
+		print "mimicking text from file: \n  " + filename
+		mimic_style(filename)
+	else:
+		print "unknown option: " + option
+		sys.exit(1)
 
 if __name__ == "__main__":
 	"""If called as script, then run main() as expected.  Else, run as module
