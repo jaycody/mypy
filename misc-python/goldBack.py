b9d727683faae38c738cf9a9d1d7d2c3c6f993a1
Jason Stephens <jason@catalogspree.com>
2014-07-29 00:47:12 -0700

    started the GoldBack conjecture script but am leaving it for fresher times

---
+++ misc-python/goldBack.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python -tt
+
+"""jstephen - pyfu - 2014 july
+
+Test the GoldBack Conjecture.
+
+This script tests the GoldBach conjecture for all integers between 4-100.
+- Every even number greater than 2 can be expressed by the sum of two primes
+- For every even number between 4-100, print the 2 primes whose sums = that even number
+- Clues: 
+	1. generate a list of primes
+	2. add each prime to each other prime
+- Use Big-O notation to express the time complexity
+"""
+
+# FOR LATER
+
+
