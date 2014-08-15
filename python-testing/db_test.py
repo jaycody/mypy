53a7a4f3e699127277c6356e2cec6e23a82b5c3d
Jason Stephens <jason@catalogspree.com>
2014-08-14 21:09:23 -0700

    created unittest fixtures and control flow tracer utility method

---
+++ python-testing/db_test.py
@@ -0,0 +1,9 @@
+#!usr/bin/env python -tt
+
+""" jstephens - unittest exercises - db and backend tests
+"""
+
+
+import unittest
+
+
---
+++ python-testing/fixture-fail-unittest.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python -tt
+
+""" jstephens - pyfu - unittest test fixture failure and exception handling 
+
+from Brian Okken's post on test fixture failure
+http://pythontesting.net/framework/unittest/when-unittest-fixtures-fail/
+"""
+
+
+from  traceFlow_fixtures_unittest import TestFlowFixtures, setUpModule, tearDownModule
+
+
+
