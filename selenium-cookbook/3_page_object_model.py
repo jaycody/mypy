e57e84897de3dad3e45bf76630b6f518789611cc
Jason Stephens <jason@catalogspree.com>
2014-08-16 20:41:17 -0700

    created Page Object Model example

---
+++ selenium-cookbook/3_page_object_model.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python -tt
+
+""" jstephens - pyfu - unittest selenium via PageObjectModel abstraction
+
+Reduce duplication - Add abstraction - Hide inner implementation
+"""
+
+
+import unittest
+
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+class TestPageOjbectModel(unittest.TestCase):
+
+	def setUp(self):
+
+
+	def tearDown(self):
+
+
+	def test_unknown(self):
+
+
+
+
+
+
+if __name__ == '__main__':
+
+	unittest.main()
+
+
+
+
+	
+
