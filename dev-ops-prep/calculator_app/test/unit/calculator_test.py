7dd3c09ba7334529fb32ba8eadfdeb78ebdedfa8
Jason Stephens <videoalchemy@gmail.com>
2017-10-28 18:16:28 -0700

    unittest this

---
+++ dev-ops-prep/calculator_app/test/unit/calculator_test.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python -tt
+
+import unittest
+
+#from calculator import Calculate
+import calculator
+
+class TestCalculate(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = Calculate()
+
+    def test_add_inputs_returns_correct_result(self):
+        self.assertEqual(4, self.calc.add_inputs(2,2))
+
+if __name__ == '__main__':
+    unittest.main()
