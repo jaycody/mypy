0f0092159008ba7a6da44b0f06e4a526ceb236ae
Jason Stephens <videoalchemy@gmail.com>
2017-10-29 00:51:52 -0700

    nose2 tests an updated reamde

--- dev-ops-prep/README.md
+++ dev-ops-prep/README.md
@@ -2,6 +2,21 @@
 > gettin' Pythonic up in this mug
 __
 
+## Unit testing with nose2
+[nose2 docs](http://nose2.readthedocs.io/en/latest/usage.html)  
+### Run individual tests
+- to run individual test script, pass in test script name using dot notation 
+     - ```nose2 calculator_app.test.unit.test_calculate_this```
+- to run individual test cases, use ':' and specify the index of the test case (NOTHING YET)
+     - ```nose2 calculator_app.test.unit.test_calculate_this:1``` 
+
+### nose2 options
+```nose2 -v  #verbose```
+```nose2 -h  #help```
+```nose2 -s  #start directory```
+```nose2 -t  #top directory```
+
+
 ## Exercises
 ### as they come to mind:
 - [x] helloworld, oh yeah
---
+++ dev-ops-prep/calculator_app/test/unit/test_calculate_this.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python -tt
+
+# usage from calculator_app/: python test/unit/calculate_this_test.py
+
+import unittest
+
+from calculate_this import Calculate
+
+
+class TestCalculate(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = Calculate()
+
+
+    ###############################
+    ### verify it works as expected
+
+    def test_add_inputs_returns_correct_result(self):
+        self.assertEqual(4, self.calc.add_inputs(2, 2))
+
+
+    ###############################
+    ### verify it breaks as expected
+
+    def test_add_inputs_raises_type_error_for_non_ints(self):
+        self.assertRaises(TypeError, self.calc.add_inputs, 'Hello', 'Again')
+
+    def test_add_inputs_raises_attribute_error_on_non_existent_method_call(self):
+        with self.assertRaises(AttributeError):
+            [].get
+
+
+if __name__ == '__main__':
+    unittest.main()
