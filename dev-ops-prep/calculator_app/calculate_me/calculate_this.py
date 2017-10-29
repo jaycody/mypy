f3423e7f94e54de10fc8ec8617339c7a59a98c6a
Jason Stephens <videoalchemy@gmail.com>
2017-10-28 22:10:41 -0700

    validate type else raise TypeError

--- dev-ops-prep/calculator_app/calculate_me/calculate_this.py
+++ dev-ops-prep/calculator_app/calculate_me/calculate_this.py
@@ -2,8 +2,11 @@
 
 class Calculate(object):
     def add_inputs(self, x, y):
-        return x+y
-
+        if (type(x) and type(y)) == int:
+            return x+y
+        else:
+            raise TypeError('Invalid Types. Expected Ints. Got {} and {}'
+                            .format(type(x), type(y)))
 
 
 if __name__ == '__main__':
--- dev-ops-prep/calculator_app/test/unit/calculate_this_test.py
+++ dev-ops-prep/calculator_app/test/unit/calculate_this_test.py
@@ -1,5 +1,7 @@
 #! /usr/bin/python -tt
 
+# usage from calculator_app/: python test/unit/calculate_this_test.py
+
 import unittest
 
 from calculate_this import Calculate
@@ -12,7 +14,10 @@ class TestCalculate(unittest.TestCase):
         self.calc = Calculate()
 
     def test_add_inputs_returns_correct_result(self):
-        self.assertEqual(4, self.calc.add_inputs(2,2))
+        self.assertEqual(4, self.calc.add_inputs(2, 2))
+
+    def test_add_inputs_raises_type_error_for_non_ints(self):
+        self.assertRaises(TypeError, self.calc.add_inputs, 'Hello', 'Again')
 
 if __name__ == '__main__':
     unittest.main()
