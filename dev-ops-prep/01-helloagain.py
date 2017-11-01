507543bf85d81b670c06380b8c480f82b2f55d04
Jason Stephens <videoalchemy@gmail.com>
2017-10-31 22:51:31 -0700

    devops dir (package) name is now PEP8 compliant; removed dashes

--- dev-ops-prep/01-helloagain.py
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/python -tt
-
-
-for i in range(100):
-    print ('helloworld')
---
+++ devops/aahelloagain.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python -tt
+
+
+def print_helloworld_to_console():
+    for i in xrange(100):
+        print ('helloworld')
+
+
+if __name__ == '__main__':
+    print_helloworld_to_console()
+		
+
