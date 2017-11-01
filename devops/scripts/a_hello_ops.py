073698cb335e0ac8815ec9f4cb5d3784fd2b8796
Jason Stephens <videoalchemy@gmail.com>
2017-11-01 01:04:34 -0700

    added argv to hello ops repeat print

--- devops/scripts/a_hello_ops.py
+++ devops/scripts/a_hello_ops.py
@@ -1,12 +1,16 @@
 #! /usr/bin/python -tt
 
+import sys
 
-def print_helloworld_to_console():
-    for i in xrange(100):
-        print ('helloworld')
+def print_hello_devops_to_console():
+    if len(sys.argv) > 1:
+        repeat_print = int(sys.argv[1])
+    else:
+        repeat_print = 100
+
+    for i in xrange(repeat_print):
+        print 'hello devops ', i
 
 
 if __name__ == '__main__':
-    print_helloworld_to_console()
-		
-
+    print_hello_devops_to_console()
