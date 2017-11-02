b77c23f7805bf40300efad0e42c61c18fe95cb5b
Jason Stephens <videoalchemy@gmail.com>
2017-11-02 03:57:23 -0700

    successive addition with a recursive function

---
+++ devops/scripts/f_recur_mul.py
@@ -0,0 +1,22 @@
+#! usr/bin/pyhthon -tt
+
+'''
+recursive function that returns the product of two multipliers
+'''
+
+import sys
+
+def recur_mul(multiply_this, this_many_times):
+
+    # base case
+    if this_many_times == 1:
+        return multiply_this
+
+    # recursive step
+    else:
+        return multiply_this + recur_mul(multiply_this, this_many_times - 1)
+
+if len(sys.argv) > 2:
+    print recur_mul(int(sys.argv[1]), int(sys.argv[2]))
+else:
+    print recur_mul(3, 5)
