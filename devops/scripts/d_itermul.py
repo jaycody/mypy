9f0035cc3e4ec50de2d783d0aa08ad95c6a93438
Jason Stephens <videoalchemy@gmail.com>
2017-11-02 02:46:26 -0700

    iterative functions that calc mutliplication with successive addition and exponents with successive multiplication

---
+++ devops/scripts/d_itermul.py
@@ -0,0 +1,14 @@
+#! usr/bin/python -tt
+
+"""itermul - interative multiplication by successive addition"""
+
+
+
+def itermul(multiply_this, by_this):
+    result = 0
+    for multiple in xrange(by_this):
+        result = result + multiply_this
+    return result
+
+
+print itermul(7, 3)
---
+++ devops/scripts/e_iterexp.py
@@ -0,0 +1,39 @@
+#! usr/bin/python -tt
+
+# iterative functions that calculates the exponential base using successive multiplication
+
+import sys
+
+def iterexp(base, exp):
+    '''
+    base: int or float
+    exp: int >= 0
+
+    returns: int or float, base^exp
+    '''
+
+    results_from_iterexp = 0
+    for i in xrange(exp):
+        results_from_iterexp = itermul(base, base) + results_from_iterexp
+    return results_from_iterexp
+
+
+def itermul(add_this_to_itself, this_many_times):
+    '''
+    iterative multiplication via successive addition
+    For each of the exp loops in the iterexp function, a call is made to the itermul function
+    such that successive addition is used to find a product, and successive calls to this function are used to caclulate an exponent via successive multiplication
+    '''
+ 
+    results_from_itermul = 0
+    for i in xrange(this_many_times):
+        results_from_itermul = add_this_to_itself + results_from_itermul
+    return results_from_itermul
+        
+
+if len(sys.argv) == 3:
+    final_result = iterexp(int(sys.argv[1]), int(sys.argv[2]))
+else:
+    final_result = iterexp(3, 3)
+
+print final_result
