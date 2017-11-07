e2719c6c5162a1de50acd9d5d90668cd146032f5
Jason Stephens <videoalchemy@gmail.com>
2017-11-06 17:39:39 -0800

    Lo\! and behold. We have function query capabilities.

--- devops/scripts/q_lenstr.py
+++ devops/scripts/q_lenstr.py
@@ -5,7 +5,7 @@ Validates data type first.
 """
 
 
-def fast_string_length(s):
+def fast_string_length(s="requires input"):
     """Counts and returns the elements in a string.
     Walks throught the string with a for in each iteratior
     """
@@ -23,3 +23,12 @@ print "\nTest_string: '" + test_string + "' has element count of ", fast_string_
 test_string = "ba;alsdjga;jga;jga;jgajgajgdjrj"
 print "\nTest_string: '" + test_string + "' has element count of ", fast_string_length(test_string)
 print 'Verified length: ', len(test_string)
+
+
+print '\nTest Default value for fast_string_length function:'
+print 'Default string, ' + fast_string_length.func_defaults[0] + "', has element count: ", fast_string_length()
+
+print '\nFunction name: ', fast_string_length.func_name
+#print fast_string_length.func_code
+print '\nDefault values: ',  fast_string_length.func_defaults
+#print fast_string_length.func_globals
