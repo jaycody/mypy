ce17abcd1f871a3a13c84228db9586f8379a3a88
Jason Stephens <videoalchemy@gmail.com>
2017-11-06 16:21:31 -0800

    changed docstring single quotes to triple double quites as per PEP8

--- devops/scripts/p_palindrome.py
+++ devops/scripts/p_palindrome.py
@@ -1,8 +1,7 @@
 #! /usr/bin/python -tt
 
-'''
-Determines if a given string is a Palindrome
-'''
+""" Determines if a given string is a Palindrome.
+"""
 
 def isPalindrome(string_to_check):
     #print "steps:"
@@ -12,10 +11,9 @@ def isPalindrome(string_to_check):
     #print "\t3: isPal compares first and last chars in the string. Returns true if Palindrome"
 
     def toChar(chars_to_clean):
-        '''
-        Removes all whitespace and punctuation from a string
-        Returns a squeeky clean version of the string in lowercase
-        '''
+        """ Removes all whitespace and punctuation from a string.
+        Returns a squeeky clean version of the string in lowercase.
+        """
 
         ans = ''
 
@@ -27,13 +25,18 @@ def isPalindrome(string_to_check):
 
 
     def isPal(s):
+        """ Recursively checks the first and last letters of a string.
+        Returns True if string is palindrome.
+        """
 
         # base case
+        ## MAKE PEP 8 COMPLIANT: EMPTY SEQUENCES EVALUATE TO FALSE. USE: if not mylist:
         if len(s) == 0 or len(s) == 1:
             print '\nYES! String: ' + string_to_check + ' is a Palindrome\n'
             return True
 
         # compare first and last chars:
+        #### MAKE PEP8 COMPLIANT BY REPLACING SLICES WITH STRING METHODS startswith(), endswith
         if s[0] == s[-1]:
             #print 'recursive case! String is now: ' + s
             return isPal(s[1:-1])
