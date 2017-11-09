dc69dc344c4d3191441cd83a1d999b663f66714a
Jason Stephens <videoalchemy@gmail.com>
2017-11-09 15:58:53 -0800

    decimal to binary converter

---
+++ devops/scripts/t_dec_to_binary.py
@@ -0,0 +1,49 @@
+#! /usr/bin/python -tt
+
+"""Converts base 10 positive ints into binary
+"""
+
+
+def convert_to_binary(x):
+    """Convert to binary any positive int x of base 10.
+
+    x: positve int
+    steps:
+    0. convert decimal into base 10 representation
+        eg 40932 = 4 * 10**4 + 0 * 10**3 + 9 * 10**2 + 3 * 10**1 + 2 * 10**0
+    1. check x % 2 for initial binary digit
+    2. Right shift x by dividing by 2
+    3. check new x % 2 for next binary digit
+    4. repeat until len x == 0
+    5. return binary digit
+    """
+
+
+    return x
+
+
+
+
+
+
+def test_convert_to_binary():
+    """validate the convert_to_binary function
+
+    test for expected success
+    test for various failures
+    """
+
+    # test 1: do you exist?
+    t1 = 4
+    return "If you exist, return {} ---> {} returned".format(t1, convert_to_binary(t1))
+
+
+
+
+
+
+
+print test_convert_to_binary()
+
+
+#print convert_to_binary('10')
