ff19fcc554bde2cd56c80e5005b68b7be1999cd5
Jason Stephens <videoalchemy@gmail.com>
2017-11-10 23:54:01 -0800

    oh, actually that range evaluation for equivalency needs a ZERO, otherwise fizzbuzz prints for num like 473327 where modulo for 3 and 5 are equal, just not equal to ZERO. (2 instead)

--- devops/scripts/v_fizzbuzz.py
+++ devops/scripts/v_fizzbuzz.py
@@ -8,22 +8,25 @@ def fizzbuzz(x):
 
     # print fizz and buzz if modulo 5 & 3 both return 0
     # NICE, this check range thing actually worked!!!
-    if x % 3 == x % 5:
-        return 'fizzbuzz'
+    if x % 3 == 0 == x % 5:
+        return 'fizzbuzz!  when x={}'.format(x)
 
     #Print fizz when val module 3 = 0
-    if x % 3 == 0:
+    elif x % 3 == 0:
         return 'fizz'
 
     #Print fizz when val module 3 = 0
-    if x % 5 == 0:
+    elif x % 5 == 0:
         return 'buzz'
 
+    else:
+        return None
 
 
 
-def test_fizzbuzz():
+def test_fizzbuzz(x=15):
     """Test the fizzbuzzness"""
+
 
     ## Test if module 3 remainder 0 prints fizz
     print fizzbuzz(3)
@@ -34,9 +37,14 @@ def test_fizzbuzz():
     ## test if modulo 5 and 3 = 0 prints fizzbuzz
     print fizzbuzz(15)
 
+    #okay, lets check the full range
+    for i in xrange(x):
+        if fizzbuzz(i) is not None:
+            print fizzbuzz(i)
 
-
-
+# test default value
 test_fizzbuzz()
 
-#print fizzbuzz.func_name
+# test big num
+test_this = 99999999999
+test_fizzbuzz(test_this)
