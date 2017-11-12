242f48c099ccf892576aa1ba2057e09ca9e549bd
Jason Stephens <videoalchemy@gmail.com>
2017-11-12 00:36:34 -0800

    test runner takes a suite of test cases, unpacks, iterates, runs the tests, compares got against expected and reports the test results

--- devops/gscripts/junc_tester.py
+++ devops/gscripts/junc_tester.py
@@ -19,31 +19,43 @@ def test(got, expected):
         result = 'PASS'
     else:
         result = ' X'
-    return '{:10s} got: {:15s} expected: {}'.format(result, repr(got), repr(expected))
+    return '  {:10s} got: {:15s} expected: {}'.format(result, repr(got), repr(expected))
 
+def test_runner(full_test_suite):
+    """Extract and run each test case from the full test suite"""
+
+    for f_test in full_test_suite:
+        print '\nTesting: {}'.format(f_test[0])
+        for got, expected in f_test[1]:
+            print test(got, expected)
 
 if __name__ == '__main__':
 
-
-    # try the map function?
-
-    func1_values = [(do(10), 10),
+    full_test_suite = []
+    ##############################
+    ## f1 test info
+    f1_name = 'do()'
+    f1_values = [(do(10), 10),
                     (do(10), 100),
                     (do(99), 99)
                     ]
+    f1 = (f1_name, f1_values)
+    ## append f1's test info to the test_suite
+    full_test_suite.append(f1)
+    ##############################
 
-    func2_values = [(do_that(120), 120),
+    ##############################
+    ## f2 test info
+    f2_name = 'do_that()'
+    f2_values = [(do_that(120), 120),
                     (do_that(14430), 13300),
                     (do_that(9900), 9900)
                     ]
+    f2 = (f2_name, f2_values)
+    ## append f2 test info to the test suite
+    full_test_suite.append(f2)
+    ##############################
 
-    test_suite = [func1_values,
-                func2_values]
-
-    for test_values in test_suite:
-        for got, expected in test_values:
-            print test(got, expected)
-
-
-#    for got, expected in func2_values:
-#        print test(got, expected)
+    ##############################
+    ## run that thing
+    test_runner(full_test_suite)
