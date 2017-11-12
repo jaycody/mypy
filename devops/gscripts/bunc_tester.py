dcf7b77cc8348187dc8361cc5a4ba07dd34030bb
Jason Stephens <videoalchemy@gmail.com>
2017-11-11 21:58:52 -0800

    format parameters for Homie McHomeboy. He likes it... he really really likes it. especially unpacking the args special tuple

--- devops/gscripts/bunc_tester.py
+++ devops/gscripts/bunc_tester.py
@@ -30,9 +30,19 @@ def test(got=None, expected=None):
 
 def main():
     #print check_if('to check'),
-    print 'tests:'
-    print test(check_if('sent_to_test_and_w'), 'sent_as_expected')
-    print test(check_if('got_expected'), 'got_expected')
+    #print 'tests:'
+    #print test(check_if('sent_to_test_and_w'), 'sent_as_expected')
+    #print test(check_if('got_expected'), 'got_expected')
+
+    ##### FORMAT!!  #####
+    res = ['Homie McHomeboy', 35,  24.798]  # print this
+    args = (10, 5, 2.2)                     # with these dimensions
+    print('{0[0]:{1}s} {0[1]:{2}d} {0[2]:{3}f}'.format(res, *args))
+    ## where:
+    ##  's' # format a STRING with n spaces, left justified by defau
+    ##  'd' # format an INT reserving n spaces, right justified by default
+    ##  'f' # format a float, reserving n spaces, and .n spaces after the decimal point, right justfied by default.
+    ##  AND UNPACK THE *args
 
 if __name__ == '__main__':
     main()
