806167a0657c60ea79b17682496c54e98d33af01
Jason Stephens <videoalchemy@gmail.com>
2017-11-11 23:39:32 -0800

    test running off for each looped tuple in a list of test values

--- devops/gscripts/junc_tester.py
+++ devops/gscripts/junc_tester.py
@@ -6,6 +6,9 @@
 def do(x):
     return x
 
+def do_that(x):
+    return x
+
 def test(got, expected):
     """Compare what ya got with the truth.
     Tell me straight: was is it everything you expected it be?
@@ -15,11 +18,27 @@ def test(got, expected):
     if got == expected:
         result = 'PASS'
     else:
-        result = 'X'
+        result = ' X'
     return '{:10s} got: {:15s} expected: {}'.format(result, repr(got), repr(expected))
 
 
 if __name__ == '__main__':
 
 
-    print test(do(10), 10)
+    # try the map function?
+
+    func1_values = [(do(10), 10),
+                    (do(10), 100),
+                    (do(99), 99)
+                    ]
+
+    func2_values = [(do_that(120), 120),
+                    (do_that(14430), 13300),
+                    (do_that(9900), 9900)
+                    ]
+
+    test_suite = [func1_values,
+                func2_values]
+
+    for got, expected in func2_values:
+        print test(got, expected)
